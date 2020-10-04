360bdbb949725246c4c6c0f99744dc11a051177c
Michelle Scott <scottmm374@gmail.com>
2020-10-04 10:58:00 -0400

    just made some comments on how to structure

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -25,6 +25,7 @@ class HashTable:
 
     def __init__(self, capacity):
         self.hash_list = [None] * capacity
+        # update capacity when resizing, then we can just re use hash_list I think.
         self.capacity = 8
         self.occupied_slots = 0
         self.head = None
@@ -35,7 +36,7 @@ class HashTable:
 
     def get_load_factor(self):
 
-        LF = self.occupied_slots / self.get_num_slots()
+        LF = self.occupied_slots / self.capacity
         print(LF, "Current Load Factor")
         return LF
 
@@ -53,38 +54,55 @@ class HashTable:
         return self.djb2(key) % len(self.hash_list)
 
     def put(self, key, value):
-        # Array version
-        self.hash_list[self.hash_index(key)] = value
 
-    def insert(self, key, value):
-        """
-        # Search the linked list for a Node with the same KEY as the one we are inserting
-                # If it exists:
-                    # change the value of the node
-                    # return
-            # if it doesnt exist do the following steps
-​
-            # the first item in the hash_list is the HEAD of the linked list
-            # Create a new hashTableEntry and add it to the HEAD of the linked list
-            # Make the new entry the new HEAD
-        """
+        new_key = self.hash_list[self.hash_index(key)] = value
+        # Check load Factor first
+        loadFactor = self.get_load_factor()
+           if loadFactor > Min_LF and loadFactor < Max_LF:
+                # * If load factor good:
+
+                      # check to see if index occupied, can we check id Head is None:
+                          # * if not occupied then insert:
+                          new_key = self.head
+                          return self.put(key, value)
+
+                          # ! If occupied:
+                        #   current.head = key at index.
+                        # new_key insert and = head
+                        # current_head = head.next
+
+            # ! If load Factor not good:
+
+            return self.put(key, value)
+
 
     def delete(self, key):
 
-        # Array version
-        if self.hash_list[self.hash_index(key)] is None:
+        key_search = self.hash_list[self.hash_index(key)]
+
+        # Search first to see if it exists using index(if its occupied)
+
+            # if index is None:  (check below, it may not check quite like I need to)
+            #     Key not found
+            if self.hash_list[self.hash_index(key)] is None:
             print("key not found")
+
+            # if Index occupied, BUT Key doesnt match:    CAN PROB DO ALL THIS IN FIND()
+            #     Iterate using next, to find key
+            #         if not found:
+            #             return not found
+            #         if Found :
+            #             value = none (delete)
+            #             subtract 1 from occupied slots.
+            # Check load factor, for being too small, if it is too small resize, smaller but min 8 slots.  
+        
         else:
             self.hash_list[self.hash_index(key)] = None
 
         """
         # Search through the linked list until we find the node to delete
         # Delete the node if found
-
-        """
-
-    def delNode(self, key):
-        """
+     
         # Search the linked list for a Node with the same KEY as the one we are inserting
                 # If it exists:
                     # change the value of the node
@@ -94,7 +112,7 @@ class HashTable:
             # the first item in the hash_list is the HEAD of the linked list
             # Create a new hashTableEntry and add it to the HEAD of the linked list
             # Make the new entry the new HEAD
-        """
+     """
 
     def get(self, key):
         # Array version
