fb8c938bd9b1e4800a70556aca24de6180732cf0
Michelle Scott <scottmm374@gmail.com>
2020-10-06 11:59:04 -0400

    have it down to listing all words as value in a list

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,34 @@
+s = 'Hello, my cat.  And my cat doesn\'t say "hello" back.'
+
+
 def word_count(s):
-    pass
+
+    count = {}
+    # removes special characters from string
+    remove_Special_Chars = s.translate(
+        {ord(c): " " for c in "\"!@  # $%^&*()[]{};:,./<>?|`~-=_+"}).split()
+
+    for word in remove_Special_Chars:
+        words = word
+
+        if word not in count:
+            count[words] = [words]
+        else:
+            count[words].append(word)
+
+    return count
+
+    # if __name__ == "__main__":
+    #     print(word_count(""))
+    #     print(word_count("Hello"))
+    #     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+    #     print(word_count(
+    #         'This is a test of the emergency broadcast network. This is only a test.'))
+    # word_count('":;,.-+=/\\|[]{}()*^&')
 
 
-if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count(
-        'This is a test of the emergency broadcast network. This is only a test.'))
+x = word_count(s)
+print(x)
+
+
+# s = 'Hello, my cat.  And my cat doesn\'t say "hello" back.'
