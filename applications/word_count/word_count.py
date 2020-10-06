53b66fd1256c4909036012a600453ef40c5a13d9
Michelle Scott <scottmm374@gmail.com>
2020-10-06 12:50:09 -0400

    test passes, dont think its optimized though, Word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,4 +1,4 @@
-s = 'Hello, my cat.  And my cat doesn\'t say "hello" back.'
+# s = 'Hello, my cat.  And my cat doesn\'t say "hello" back.'
 # s = '":;,.-+=/\\|[]{}()*^&'
 
 
@@ -18,17 +18,17 @@ def word_count(s):
 
     return word_dict
 
-    # if __name__ == "__main__":
-    #     print(word_count(""))
-    #     print(word_count("Hello"))
-    #     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    #     print(word_count(
-    #         'This is a test of the emergency broadcast network. This is only a test.'))
-    # word_count('":;,.-+=/\\|[]{}()*^&')
+    if __name__ == "__main__":
+        print(word_count(""))
+        print(word_count("Hello"))
+        print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+        print(word_count(
+            'This is a test of the emergency broadcast network. This is only a test.'))
+    word_count('":;,.-+=/\\|[]{}()*^&')
 
 
-x = word_count(s)
-print(x)
+# x = word_count(s)
+# print(x)
 
 
 # s = 'Hello, my cat.  And my cat doesn\'t say "hello" back.'
