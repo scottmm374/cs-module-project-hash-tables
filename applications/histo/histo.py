82d08846012185d9409bd92c08439cc34291e63b
Michelle Scott <scottmm374@gmail.com>
2020-10-06 21:33:46 -0400

    most of histo done, need to print hashes

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,24 @@
-# Your code here
 
+
+with open("robin.txt") as f:
+    words = f.read()
+
+
+# def hist(words):
+    histo_dict = {}
+
+  # removes special characters from string
+    remove_Special_Chars = words.translate(
+        {ord(c): " " for c in "\"!@  # $%^&*()[]{};:,./\\<>?|`~-=_+"}).lower().split()
+
+    for word in remove_Special_Chars:
+        words = word
+
+    # if the string does not exist yet
+        if word not in histo_dict:
+            number = remove_Special_Chars.count(word)
+            histo_dict[words] = number
+
+    print(histo_dict)
+    # return word_dict
+# hist()
