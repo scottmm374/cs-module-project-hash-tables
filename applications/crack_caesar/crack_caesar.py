4e513ae684cfa0cf0402d6a55d1649bb3a6c0d48
Michelle Scott <scottmm374@gmail.com>
2020-10-08 11:41:21 -0400

    skipped non alpha characters in dict, ceasar

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,11 +1,8 @@
 import time
 
 
-# crypt_list = []
-
 with open("ciphertext.txt") as f:
     words = f.read()
-    # crypt_list.append(words)
 
 
 def ceasar(letters):
@@ -13,14 +10,20 @@ def ceasar(letters):
     crypto_dict = {}
 
     for char in letters:
-        letter = char
-        # print(char, "characters")
+        # skip non alpha characters
+        if char.isalpha():
+
+            letter = char
 
         if char not in crypto_dict:
             number = words.count(char)
             crypto_dict[letter] = number
-
+    print(len(crypto_dict))
     return crypto_dict
 
 
 print(ceasar(words))
+
+start = time.time()
+end = time.time()
+print(f"Calculating Ceasar {(end - start):.10f} seconds")
