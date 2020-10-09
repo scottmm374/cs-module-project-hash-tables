d83121566e406ec2bb66776fc7c5a47801cb1cc8
Michelle Scott <scottmm374@gmail.com>
2020-10-09 12:34:41 -0400

    decode works, but no spaces in between words

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -7,14 +7,9 @@ with open("ciphertext.txt") as f:
 decrypt_list = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W',
                 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
 
-decode_table = {}
-
-
-def decode(decodeList, dict):
-    pass
-
 
 def ceasar(letters):
+    decrypt_tup = list(enumerate(decrypt_list))
 
     crypto_dict = {}
 
@@ -28,15 +23,45 @@ def ceasar(letters):
         if char not in crypto_dict:
             number = words.count(char)
             crypto_dict[letter] = number
-
+    # sorting dict by occurance of letters decending
     sorted_dict = sorted(
         crypto_dict.items(), key=lambda x: x[1], reverse=True)
-    print(len(crypto_dict))
-    return sorted_dict
+    # print(len(crypto_dict))
+
+    # creating dictionary with 1st index tup(as key) of sorted, and decrypt_tup as value, based on frequesncy of letter.
+
+    decode_dict = {sorted_dict[i][0]: decrypt_tup[i][1]
+                   for i, _ in enumerate(decrypt_tup)}
+
+    encrypted_text = ""
+
+    for char in words:
+        if char.isalpha():
+            # if char != char.translate(
+            #         {ord(c): " " for c in "\"!@  # $%^&*()[]{};:,./\\'\n'<>?|`~-=_+"}) or char == (' '):
+            #     continue
+            encrypted_text += decode_dict[char]
+
+    return encrypted_text
+
+    # print("Sorted_dict: \n", sorted_dict)
+    # # print("crypto_dict: \n", crypto_dict)
+    # # print("Decrypt_tuple: \n", decrypt_tup)
+    # print("decode_dict: \n", decode_dict)
+    # return decode_dict
+
+
+# def decode_txt(text):
+#     encrypted_text = ""
+
+#     for char in text:
+#         encrypted_text += ceasar(words[char])
+
+#     return encrypted_text
 
 
 print(ceasar(words))
 
-start = time.time()
-end = time.time()
-print(f"Calculating Ceasar {(end - start):.10f} seconds")
+# start = time.time()
+# end = time.time()
+# print(f"Calculating Ceasar {(end - start):.10f} seconds")
