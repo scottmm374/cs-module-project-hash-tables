426281615e085bdf42c5ad80e9e1eeb4ad5500ed
Michelle Scott <scottmm374@gmail.com>
2020-10-10 12:54:15 -0400

    cracked ceasar with propoer spacing and punctuation

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,3 +1,4 @@
+
 import time
 
 
@@ -9,59 +10,42 @@ decrypt_list = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W',
 
 
 def ceasar(letters):
+    # Creating tuple list to combine with crypto dict based off frequency. Dycrypt list in order from most frequest to least.
     decrypt_tup = list(enumerate(decrypt_list))
 
     crypto_dict = {}
 
+# Adding txt file characters to dictionary
     for char in letters:
-        # skip non alpha characters
-        if char != char.translate(
-                {ord(c): " " for c in "\"!@  # $%^&*()[]{};:,./\\'\n'<>?|`~-=_+"}) or char == (' '):
-            continue
-        letter = char
+        if char.isalpha():
+            letter = char
 
-        if char not in crypto_dict:
-            number = words.count(char)
-            crypto_dict[letter] = number
-    # sorting dict by occurance of letters decending
+            if char not in crypto_dict:
+                number = words.count(char)
+                crypto_dict[letter] = number
+
+        # sorting dict by occurance of letters decending
     sorted_dict = sorted(
         crypto_dict.items(), key=lambda x: x[1], reverse=True)
-    # print(len(crypto_dict))
 
     # creating dictionary with 1st index tup(as key) of sorted, and decrypt_tup as value, based on frequesncy of letter.
 
     decode_dict = {sorted_dict[i][0]: decrypt_tup[i][1]
                    for i, _ in enumerate(decrypt_tup)}
 
+#    Where the Magic happens, decoding text
+
     encrypted_text = ""
 
     for char in words:
         if char.isalpha():
-            # if char != char.translate(
-            #         {ord(c): " " for c in "\"!@  # $%^&*()[]{};:,./\\'\n'<>?|`~-=_+"}) or char == (' '):
-            #     continue
             encrypted_text += decode_dict[char]
 
+        else:
+            encrypted_text += char
+
+    print(encrypted_text, "encryted txt")
     return encrypted_text
 
-    # print("Sorted_dict: \n", sorted_dict)
-    # # print("crypto_dict: \n", crypto_dict)
-    # # print("Decrypt_tuple: \n", decrypt_tup)
-    # print("decode_dict: \n", decode_dict)
-    # return decode_dict
 
-
-# def decode_txt(text):
-#     encrypted_text = ""
-
-#     for char in text:
-#         encrypted_text += ceasar(words[char])
-
-#     return encrypted_text
-
-
-print(ceasar(words))
-
-# start = time.time()
-# end = time.time()
-# print(f"Calculating Ceasar {(end - start):.10f} seconds")
+ceasar(words)
